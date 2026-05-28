09f892580740643c6ec74eb1797d9b1e7c6f6fc5
Edouard <edouardtuyubahe@gmail.com>
2026-05-28 15:29:08 +0200

    Doing practice on operations and some functions

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 import numpy as np
 
 a = np.array([1,2,3])
+b = (a+2)
+c = [1,2,3,4]
+
+print(c)
+print(a)
+print(b)
+print(a + b)
+
+# creating a 2D array
+d = np.array([
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+])
+
+print()
+print()
+print(".")
+
+#printing a 3D
+print(d)
+
+# TOview the shape of your table
+print(d.shape)
+print(d.ndim)
---
+++ practice.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+a = np.array([5,10,15])
+
+print(a);
+print(a + 10)
+print(a.shape)
+print(a.ndim)
